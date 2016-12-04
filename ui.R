50a09e3495889317351436ad071e2e20d1f41627
suilou <shuang@air-worldwide.com>
2016-12-04 00:35:57 -0500

    update

--- ui.R
+++ ui.R
@@ -29,17 +29,3 @@ shinyUI(pageWithSidebar(
   )
   
 )
-
-# shinyUI(pageWithSidebar(  
-#   titlePanel("Moving Average Plot of S&P500 (6/22/2006-6/22/2016)"),  
-#   sidebarPanel(    
-#     sliderInput('n', 'Window Size (Days)', value = 60, min = 1, max = 360, step = 1),
-#     br(),
-#     p(strong(em("Documentation:",a("Moving Average of S&P500",href="https://github.com/suilou/DataScience9_DevelopingDataProducts/tree/master/app/Readme.md")))),
-#     p(strong(em("Github repository:",a("Developing Data Products - Peer Assessment Project; Shiny App",href="https://github.com/suilou/DataScience9_DevelopingDataProducts/tree/master/app"))))
-#   
-#   ), 
-#   mainPanel(    
-#     plotOutput('newMv')  
-#   )
-# ))
